1606305bb7035e222a5ef15f2ed283e3746de089
kunljha <jha711kunal@gmail.com>
2021-08-28 22:57:00 +0530

    'User' model setup

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,19 @@
+const User = require('../models/User')
+
 const signup_get = (req, res) => {
 	res.render('signup')
 }
 
-const signup_post = (req, res) => {
-	const { user, password } = req.body
-	console.log(user, password)
-	res.send('new signup')
+const signup_post = async (req, res) => {
+	const { email, password } = req.body
+
+	try {
+		const user = await User.create({ email, password })
+		res.status(201).json(user)
+	} catch (err) {
+		res.send('user not able to sign up')
+		console.log(err)
+	}
 }
 
 const login_get = (req, res) => {
@@ -14,8 +22,6 @@ const login_get = (req, res) => {
 
 const login_post = (req, res) => {
 	const { user, password } = req.body
-	console.log(user, password)
-	res.send('new login')
 }
 
 module.exports = { signup_get, signup_post, login_get, login_post }
